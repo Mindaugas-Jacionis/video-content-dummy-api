fead9323c59ca04bbf38367f626e6659ed92fb21
Mindaugas Jačionis <m.jacionis@gmail.com>
2022-12-01 18:40:34 +0200

    test crash

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const flatten = require("lodash.flatten");
+// const flatten = require("lodash.flatten");
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 3003;
@@ -11,20 +11,19 @@ const port = process.env.PORT || 3003;
 app.use(bodyParser.json()); // for parsing application/json
 app.use(cors());
 
-Object.entries(routes).forEach(([name, route]) => {
-  app.use(`/${name}`, route);
-});
+// Object.entries(routes).forEach(([name, route]) => {
+//   app.use(`/${name}`, route);
+// });
 
 app.get("/", function (req, res) {
-  const allRoutes = flatten(
-    Object.keys(routes).map((route) => {
-      return routes[route].stack.map(
-        ({ route: { path, stack } }) => `${stack[0].method}: /${route}${path}`
-      );
-    })
-  );
-
-  res.send(allRoutes.join("<br />"));
+  // const allRoutes = flatten(
+  //   Object.keys(routes).map((route) => {
+  //     return routes[route].stack.map(
+  //       ({ route: { path, stack } }) => `${stack[0].method}: /${route}${path}`
+  //     );
+  //   })
+  // );
+  // res.send(allRoutes.join("<br />"));
   // res.send("Hello World");
 });
 
